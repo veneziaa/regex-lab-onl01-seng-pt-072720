a444bbbe407525b88d0b7fd87d757c170222bbee
Venice Vargas <venicevargas1@live.com>
2020-09-02 20:23:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,13 +16,22 @@ return_array = text.split(" ")
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  first_letter = text.scan(/\w+/)[0]
+ first_letter = text.scan(/\w+/)[0]
   if (text.scan(/\./)) && (first_letter == first_letter.upcase)
     true
   else  
     false
   end
 end
+
+def first_word_capitalized_and_ends_with_punctuation_2?(text)
+  if text.match(/^[A-Z]/) && text.match(/[\.]$/)
+    true
+  else 
+    false
+  end
+end
+
 
 def valid_phone_number?(phone)
   if text.match(/^[A-Z]/) && text.match(/[\.]$/)
